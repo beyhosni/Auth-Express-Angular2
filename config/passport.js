734acb484bbabdf712cfa8359ef889b59db9fa9f
pluzzmo <r.boukeri@gmail.com>
2017-07-19 13:07:47 +0100

    [Auth-Express-Angular2]config passport strategy authentication

--- config/passport.js
+++ config/passport.js
@@ -1,3 +1,26 @@
 /**
  * Created by UpperSkills on 7/19/17.
  */
+var JwtStrategy = require('passport-jwt').Strategy;
+var ExtractJwt = require('passport-jwt').ExtractJwt;
+var Users = require('../models/Users').Users;
+var config = require('../config/config');
+
+// Setup work and export for the JWT passport strategy
+module.exports = function(passport) {
+    var opts = {};
+    opts.jwtFromRequest = ExtractJwt.fromAuthHeader();
+    opts.secretOrKey = config.secret;
+    passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
+        Users.findOne({id: jwt_payload.id}, function(err, user) {
+            if (err) {
+                return done(err, false);
+            }
+            if (user) {
+                done(null, user);
+            } else {
+                done(null, false);
+            }
+        });
+    }));
+};
